6a399d98fe4b65e9c89f46ee8ba1c56a25bb305f
Hat Dao <enjoy3013@gmail.com>
2016-04-09 19:28:34 +0700

    19:30

--- Kid REC/LoginViewController.swift
+++ Kid REC/LoginViewController.swift
@@ -14,15 +14,25 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-        let loginButton:FBSDKLoginButton = FBSDKLoginButton()
-        loginButton.center = self.view.center
-        self.view.addSubview(loginButton)
+        if FBSDKAccessToken.currentAccessToken() != nil {
+            performSegueWithIdentifier("jumpToMain", sender: self)
+        } else {
+            // Do any additional setup after loading the view.
+            let loginButton:FBSDKLoginButton = FBSDKLoginButton()
+            loginButton.center = self.view.center
+            self.view.addSubview(loginButton)
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "jumpToMain" {
+            self.presentViewController(segue.destinationViewController, animated: true, completion: nil)
+        }
     }
     
 
