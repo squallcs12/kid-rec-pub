55ad1b25c9b96bfa28c9a64095d85170a61325a9
Hat Dao <enjoy3013@gmail.com>
2016-04-09 10:14:59 +0700

    Initial Commit

---
+++ Kid RECTests/Kid_RECTests.swift
@@ -0,0 +1,36 @@
+//
+//  Kid_RECTests.swift
+//  Kid RECTests
+//
+//  Created by Hat Dao on 4/9/16.
+//  Copyright © 2016 Bang Dao. All rights reserved.
+//
+
+import XCTest
+@testable import Kid_REC
+
+class Kid_RECTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
