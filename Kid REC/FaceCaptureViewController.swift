8c591e0bcbc5572fe534e966052ef0f1cd0b9b86
Hat Dao <enjoy3013@gmail.com>
2016-04-09 13:20:09 +0700

    13:20 Finish capture image

--- Kid REC/FaceCaptureViewController.swift
+++ Kid REC/FaceCaptureViewController.swift
@@ -12,6 +12,7 @@ import AVFoundation
 class FaceCaptureViewController: UIViewController {
     @IBOutlet weak var previewView: UIView!
     @IBOutlet weak var takePhotoButton: UIButton!
+    @IBOutlet weak var capturedImage: UIImageView!
     
     var captureSession: AVCaptureSession?
     var stillImageOutput: AVCaptureStillImageOutput?
@@ -68,15 +69,29 @@ class FaceCaptureViewController: UIViewController {
     
     // MARK: - Event listener
     @IBAction func takePhotoPressed(sender: AnyObject) {
-        if let videoConnection = stillImageOutput!.connectionWithMediaType(AVMediaTypeVideo) {
-            stillImageOutput?.captureStillImageAsynchronouslyFromConnection(videoConnection, completionHandler: {
-                sampleBuffer, error in
-                var imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(sampleBuffer)
-                var dataProvider = CGDataProviderCreateWithCFData(imageData)
-                var cgImageRef = CGImageCreateWithJPEGDataProvider(dataProvider, nil, true, kCGRenderingIntentDefault)
-                var image = UIImage(CGImage: cgImageRef, scale: 1.0, orientation: UIImageOrientation.Right)
+        if ((captureSession?.running) != nil) {
+            if let videoConnection = stillImageOutput!.connectionWithMediaType(AVMediaTypeVideo) {
+                stillImageOutput?.captureStillImageAsynchronouslyFromConnection(videoConnection, completionHandler: {
+                    sampleBuffer, error in
+                    let imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(sampleBuffer)
+                    let dataProvider = CGDataProviderCreateWithCFData(imageData)
+                    let cgImageRef = CGImageCreateWithJPEGDataProvider(dataProvider, nil, true, CGColorRenderingIntent(rawValue: 0)!)
+                    let image = UIImage(CGImage: cgImageRef!, scale: 1.0, orientation: .Down)
+                    self.capturedImage.image = image
+                    
+                    self.previewView.hidden = true
+                    self.capturedImage.hidden = false
+                    self.captureSession?.stopRunning()
+                })
+            }
                 
-            })
+            takePhotoButton.setBackgroundImage(UIImage(named: "Redo"), forState: .Normal)
+        } else {
+            self.previewView.hidden = false
+            self.capturedImage.hidden = true
+            self.captureSession?.startRunning()
+            
+            takePhotoButton.setBackgroundImage(UIImage(named: "PhotoIcon"), forState: .Normal)
         }
     }
 
